55c897724380c38c3039071a164fe1a44dff9c10
HI <inoue15451116@gmail.com>
2020-09-25 12:05:59 +0900

    Revert "Merge commit '1824d6cc52b0957b3764dd50555baf767ce2406e'"
    This reverts commit 47ad8af4b891d469396cf0624d9cc7673e6925f7, reversing
    changes made to 9201e6c5d759d35e1a0d29c49afef0542935964e.

--- app/back/app/controllers/books_controller.rb
+++ app/back/app/controllers/books_controller.rb
@@ -1,5 +1,4 @@
 class BooksController < ApplicationController
-  before_action :authenticate_user!
   def index
     @book = Book.all
   end
@@ -7,21 +6,4 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
-
-  def create
-    @book = Book.new(
-      title: book_params[:title], 
-      author: book_params[:author], 
-      label: book_params[:label],
-      user_id: current_user.id
-    )
-    if @book.save
-      redirect_to('/books')
-    end
-  end
-
-  private
-    def book_params
-      params.require(:book).permit(:title, :author, :label)
-    end
 end
--- app/back/app/models/book.rb
+++ app/back/app/models/book.rb
@@ -1,5 +1,3 @@
 class Book < ApplicationRecord
   belongs_to :user
-  validates :title, :author, :label, presence: true
-  validates :title, :author, :label, length: { minimum: 1 }
 end
